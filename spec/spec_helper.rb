636d3ca13660e61aec658ba4baad7e0839f75df9
honeniq <honeniq@gmail.com>
2015-08-23 17:40:04 +0900

    Fix container name

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,6 @@ require "serverspec"
 
 set :backend, :docker
 set :docker_url, ENV["DOCKER_HOST"]
-set :docker_image, "honeniq/ttyjs"
+set :docker_image, "honeniq/infraci"
 
 Excon.defaults[:ssl_verify_peer] = false
